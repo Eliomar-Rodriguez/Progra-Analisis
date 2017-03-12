bf254566209486fe72e947d560404172d68ee189
Eliomar Rodríguez <Eliomar Rodrguez>
2017-03-12 16:19:11 -0600

    Subiendo archivos

---
+++ Proyecto Analisis/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Analisis
+{
+    /// <summary>
+    /// Clase que representara cada recuadro en el Tetravex
+    /// </summary>
+    public class Cuadro
+    {
+        public int Up { get; set; }
+        public int Down { get; set; }
+        public int Right { get; set; }
+        public int Left{get;set;} // atributos que van a representar los posibles numeros que estaran en los lados del cuadro
+
+        /// <summary>
+        /// Constructor de la clase cuadro
+        /// </summary>
+        /// <param name="up">Numero que va arriba (en el cuadro)</param>
+        /// <param name="down">Numero que va abajo (en el cuadro)</param>
+        /// <param name="right">Numero que va a la derecha (en el cuadro)</param>
+        /// <param name="left">Numero que va a la izquierda (en el cuadro)</param>
+        public Cuadro(int up, int down,int right,int left) 
+        {
+            Up = up;
+            Down = down;
+            Right = right;
+            Left = left;
+            //Console.WriteLine("Up = {0} Down = {1}", this.Up, this.Down); // manera de imprimir en orden, dependiendo del numero que se ingrese entre las llaves como una lista
+        }
+        /// <summary>
+        /// Constructor vacio, pongo -1 a todos los elementos de la vara porque no se le puede asignar null
+        /// </summary>
+        public Cuadro()
+        {
+            Up = -1;
+            Down = -1;
+            Right = -1;
+            Left = -1;
+        }
+    }
+    class Program
+    {
+        /// <summary>
+        /// Mostrara el inicio del programa, menu, etc
+        /// </summary>
+        static void Inicio()
+        {
+            Console.WriteLine("     Matrices     \n1) Correr programa y solucionar");
+        }
+        static void Main(string[] args)
+        {
+            // prueba para ver la funcionalidad de los objetos
+            Cuadro cuadro = new Cuadro();
+            cuadro.Up = 9;
+            cuadro.Right = 8;
+            Inicio();
+            Console.WriteLine("\nUp "+cuadro.Up+"\nDown"+cuadro.Down+"\nRight "+cuadro.Right+"\nLeft"+cuadro.Left);
+            Console.ReadKey();
+        }
+    }
+}
